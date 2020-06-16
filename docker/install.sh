4c7f57890a8174faa33860abb699776f1daa42bf
Jon Jagger <jon@jaggersoft.com>
2020-06-16 08:24:37 +0100

    Go back to base image gcc:8.2.0
    Using later versions of boost causes c++ cucumber a problem.
    With Boost version: 1.67.0
    you get this error...
    "/usr/include/boost/regex/v4/match_results.hpp:60:77: error: no type named 'const_reference' in 'class std::allocator<boost::sub_match<const char*> >'\n",

--- docker/install.sh
+++ docker/install.sh
@@ -2,25 +2,23 @@
 
 readonly CUKE_VERSION=v0.5
 
-apt-get update
-apt-get install --yes \
-    cmake \
-    git \
-    libgtest-dev \
-    ruby \
-    ruby-dev
+apt-get update && apt-get upgrade --yes
+apt-get install --yes ruby ruby-dev
+gem install bundler
+
+# Install googletest headers and boost
+apt-get install --yes cmake git libboost-all-dev libgtest-dev
 
 # build cucumber-cpp
 git clone https://github.com/cucumber/cucumber-cpp.git
 cd cucumber-cpp
 git checkout tags/$CUKE_VERSION
 
-gem install bundler
 bundle install
 mkdir build && (cd build && cmake -DCUKE_DISABLE_QT=on -DCUKE_ENABLE_EXAMPLES=on ..)
 cmake --build build --target install
 mv build/gmock/src/gtest-build/libg* /usr/lib
 
 # cleanup
-rm -rf /cucumber-cpp
+rm -rf cucumber-cpp
 apt-get remove --yes cmake
