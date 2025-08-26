df8f2b9bc601ffb9a120b15d1b0d784d62926a53
JonJagger <jon@kosli.com>
2025-08-26 12:37:21 +0100

    Try commands from cucumber-cpp workflows

--- docker/install.sh
+++ docker/install.sh
@@ -1,11 +1,33 @@
 #!/bin/bash -Eeu
 
 apt-get update && apt-get upgrade --yes
-apt-get install --yes ruby ruby-dev
-gem install bundler
 
-# Install googletest headers and boost
-apt-get install --yes cmake git libboost-all-dev libgtest-dev
+apt-get install --no-install-recommends \
+          cmake \
+          g++ \
+          gcovr \
+          git \
+          libasio-dev \
+          libboost-test-dev \
+          libgl1-mesa-dev \
+          libtclap-dev \
+          ninja-build \
+          nlohmann-json3-dev \
+          qt6-base-dev \
+          ruby \
+          ruby-dev
+
+gem install bundler
+bundle install
+
+# Install gtest
+git clone https://github.com/google/googletest.git
+cd googletest
+mkdir build
+cd build
+cmake ../
+cmake --build . --parallel
+sudo cmake --install .
 
 git clone https://github.com/cucumber/cucumber-cpp.git
 cd cucumber-cpp
