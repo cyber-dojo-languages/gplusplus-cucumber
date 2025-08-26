624279c607bcb9762b0d300a2f19db4794d7fe92
JonJagger <jon@kosli.com>
2025-08-26 12:28:46 +0100

    Try using commands from readme

--- docker/install.sh
+++ docker/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -Eeu
 
-readonly CUKE_VERSION=v0.7.0
-
 apt-get update && apt-get upgrade --yes
 apt-get install --yes ruby ruby-dev
 gem install bundler
@@ -9,16 +7,49 @@ gem install bundler
 # Install googletest headers and boost
 apt-get install --yes cmake git libboost-all-dev libgtest-dev
 
-# build cucumber-cpp
 git clone https://github.com/cucumber/cucumber-cpp.git
-cd cucumber-cpp
-git checkout tags/$CUKE_VERSION
 
-bundle install
-mkdir build && (cd build && cmake -DCUKE_DISABLE_QT=on -DCUKE_ENABLE_EXAMPLES=on ..)
-cmake --build build --target install
-mv build/gmock/src/gtest-build/libg* /usr/lib
+# Create build directory
+cmake -E make_directory build
 
-# cleanup
-rm -rf cucumber-cpp
-apt-get remove --yes cmake
+# Generate Makefiles
+cmake -E chdir build cmake \
+    -DCUKE_ENABLE_BOOST_TEST=on \
+    -DCUKE_ENABLE_GTEST=on \
+    -DCUKE_ENABLE_QT_6=off \
+    -DCUKE_TESTS_UNIT=on \
+    -DCUKE_ENABLE_EXAMPLES=on \
+    ..
+
+# Build cucumber-cpp
+cmake --build build
+
+# Run unit tests
+cmake --build build --target test
+
+# Run install
+cmake --install build
+
+
+#readonly CUKE_VERSION=v0.7.0
+#
+#apt-get update && apt-get upgrade --yes
+#apt-get install --yes ruby ruby-dev
+#gem install bundler
+#
+## Install googletest headers and boost
+#apt-get install --yes cmake git libboost-all-dev libgtest-dev
+#
+## build cucumber-cpp
+#git clone https://github.com/cucumber/cucumber-cpp.git
+#cd cucumber-cpp
+#git checkout tags/$CUKE_VERSION
+#
+#bundle install
+#mkdir build && (cd build && cmake -DCUKE_DISABLE_QT=on -DCUKE_ENABLE_EXAMPLES=on ..)
+#cmake --build build --target install
+#mv build/gmock/src/gtest-build/libg* /usr/lib
+#
+## cleanup
+#rm -rf cucumber-cpp
+#apt-get remove --yes cmake
