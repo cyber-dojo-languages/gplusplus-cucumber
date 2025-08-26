552f1ddf1e03bb5442a25a491f9c48784afac6f8
JonJagger <jon@kosli.com>
2025-08-26 13:11:13 +0100

    Create ghcr image, delete dead comments

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Github Action (master)](https://github.com/cyber-dojo-languages/gplusplus-cucumber/actions/workflows/main.yml/badge.svg)](https://github.com/cyber-dojo-languages/gplusplus-cucumber/actions)
 
-## display_name="C++ (g++ 15.1.0), Cucumber-cpp ?"
-## image_name="cyberdojofoundation/gpp_cucumber_gtest"
+## display_name="C++ (g++ 15.1.0), Cucumber-cpp 0.7.0"
+## image_name="ghcr.io/cyber-dojo-languages/gpp_cucumber_gtest"
 
 ![cyber-dojo.org home page](https://github.com/cyber-dojo/cyber-dojo/blob/master/shared/home_page_snapshot.png)
--- docker/install.sh
+++ docker/install.sh
@@ -54,27 +54,3 @@ cmake --build build --target test
 
 # Run install
 cmake --install build
-
-
-#readonly CUKE_VERSION=v0.7.0
-#
-#apt-get update && apt-get upgrade --yes
-#apt-get install --yes ruby ruby-dev
-#gem install bundler
-#
-## Install googletest headers and boost
-#apt-get install --yes cmake git libboost-all-dev libgtest-dev
-#
-## build cucumber-cpp
-#git clone https://github.com/cucumber/cucumber-cpp.git
-#cd cucumber-cpp
-#git checkout tags/$CUKE_VERSION
-#
-#bundle install
-#mkdir build && (cd build && cmake -DCUKE_DISABLE_QT=on -DCUKE_ENABLE_EXAMPLES=on ..)
-#cmake --build build --target install
-#mv build/gmock/src/gtest-build/libg* /usr/lib
-#
-## cleanup
-#rm -rf cucumber-cpp
-#apt-get remove --yes cmake
